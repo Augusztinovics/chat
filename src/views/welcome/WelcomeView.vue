f1e41b09b819f0b8d188a2034098511838198f5a
Augusztinovics Janos <lancing0@gmail.com>
2024-12-14 06:42:14 +0000

    layout start

---
+++ src/views/welcome/WelcomeView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <div>
+      <!-- <header>
+        <div class="wrapper">
+          <nav>
+            <RouterLink to="/">Home</RouterLink>
+            <RouterLink to="/about">About</RouterLink>
+          </nav>
+        </div>
+    </header>
+    <main>
+      <h1>Itt a kezdet</h1>
+    </main> -->
+  </div>
+</template>
