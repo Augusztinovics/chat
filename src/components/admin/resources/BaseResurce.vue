e092805c819ea39b8f00539b85d6b6ee4c252ddd
unknown <lancing0@gmail.com>
2026-01-13 09:43:08 +0100

    user resources control style

--- src/components/admin/resources/BaseResurce.vue
+++ src/components/admin/resources/BaseResurce.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="admin-resource-holder">
         <div class="admin-resource-side">
-            <button type="button" class="resource-btn">Users</button>
+            <button type="button" class="resource-btn" :class="{ 'active': resource == 'USERS' }" @click="resource = 'USERS'">Users</button>
             <button type="button" class="resource-btn">Groups</button>
             <button type="button" class="resource-btn">Group Users</button>
             <button type="button" class="resource-btn">Friends</button>
@@ -10,12 +10,22 @@
             <button type="button" class="resource-btn">Admin Users</button>
         </div>
         <div class="admin-resource-content">
-            index or details/update if possible
+            <UsersResource v-if="resource == 'USERS'" />
         </div>
     </div>
 </template>
 <script>
-    export default {
+    import UsersResource from './UsersResource.vue';
 
+    export default {
+        components: {
+            UsersResource,
+        },
+
+        data() {
+            return {
+                resource: 'USERS',
+            }
+        },
     }
 </script>
--- src/components/admin/resources/UsersResource.vue
+++ src/components/admin/resources/UsersResource.vue
@@ -1,15 +1,64 @@
 <template>
-    <div>
+    <div class="resurce">
         <div v-if="userDetail">
 
         </div>
         <div v-else>
-
+            <h2>Users</h2>
+            <div class="resurce-control">
+                <div class="password-container">
+                    <input id="search_input" type="text" v-model="searchText" :disabled="submitting" @keypress.enter="searchUsers">
+                    <button type="button" @click="searchUsers" :disabled="submitting">
+                        <span class="icon"><IconSearch /></span>
+                    </button>
+                </div>
+                <div>
+                    <select name="pag_size" id="pag_size" v-model="limit">
+                        <option value="5">5 / page</option>
+                        <option value="10">10 / page</option>
+                        <option value="15">15 / page</option>
+                        <option value="20">20 / page</option>
+                        <option value="25">25 / page</option>
+                    </select>
+                    <button class="btn-sm btn-primary" @click="fetchUsers">Fetch All Users</button>
+                </div>
+            </div>
+            <table>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>E-mail</th>
+                    <th>Language</th>
+                    <th>Country</th>
+                    <th>City</th>
+                    <th>Description</th>
+                    <th>Created At</th>
+                </tr>
+                <tr v-for="user in users">
+                    <td>{{ user.id }}</td>
+                    <td>{{ user.username }}</td>
+                    <td>{{ user.email }}</td>
+                    <td>{{ user.lg }}</td>
+                    <td>{{ user.country }}</td>
+                    <td>{{ user.city }}</td>
+                    <td>{{ user.description }}</td>
+                    <td>{{ user.created_at }}</td>
+                </tr>
+            </table>
+            <div>
+                Pagination
+            </div>
         </div>
     </div>
 </template>
 <script>
+    import IconSearch from '@/components/icons/IconSearch.vue';
+
     export default {
+        components: {
+            IconSearch,
+        },
+
         data() {
             return {
                 users: [],
@@ -18,6 +67,19 @@
                 limit: 10,
                 offset: 1,
                 totalCount: 0,
+                searchText: '',
+                submitting: false,
+            }
+        },
+
+        computed: {},
+        methods: {
+            searchUsers() {
+
+            },
+
+            fetchUsers() {
+
             }
         },
     }
